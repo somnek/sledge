ef2cf935c1e179a4af4b8e215ac4f47bf3bc5579
somnek <mumscarpetiswet@gmail.com>
2023-12-28 21:21:11 +0800

    Hash & extraction as interface

--- model.go
+++ model.go
@@ -1,85 +1,57 @@
 package main
 
 import (
-	"strings"
+	"log"
 
 	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 )
 
-var (
-	lava  = lipgloss.Color("#F56E0F")
-	void  = lipgloss.Color("#151419")
-	dust  = lipgloss.Color("#878787")
-	slate = lipgloss.Color("#262626")
-	snow  = lipgloss.Color("#FBFBFB")
-
-	styleSelected = lipgloss.NewStyle().
-			Foreground(lipgloss.Color(slate)).
-			Background(lipgloss.Color(lava)).
-			Bold(true)
-	styleNormal = lipgloss.NewStyle().
-			Foreground(lipgloss.Color(snow)).
-			Background(lipgloss.Color(slate))
-	styleBody = lipgloss.NewStyle().
-			Padding(0, 1, 0, 1)
-)
+type Record struct {
+	key  string
+	val  interface{}
+	kind string
+}
 
 type model struct {
-	table  table.Model
-	keys   []string
-	cursor int
+	table   table.Model
+	records []Record
+	cursor  int
 }
 
 func initialModel() model {
-	keys := []string{
-		"🥕 : carrot",
-		"🍎 : apple",
-		"🍐 : pear",
+	var err error
+
+	// redis
+	rdb, err := NewClient("localhost:6379", 0)
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer rdb.Close()
+
 	t := makeTable()
+	types, err := rdb.GetRecords(ctx, "*")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, t := range types {
+		if t.kind == "hash" {
+			for k, v := range t.val.(map[string]string) {
+				log.Printf("key: %s, val: %s\n", k, v)
+			}
+		} else {
+			log.Printf("key: %s, val: %s\n", t.key, t.val)
+		}
+	}
+
 	return model{
-		table:  t,
-		keys:   keys,
-		cursor: 0,
+		table:   t,
+		records: []Record{},
+		cursor:  0,
 	}
 }
 
 func (m model) Init() tea.Cmd {
 	return nil
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "ctrl+c":
-			return m, tea.Quit
-		case "j", "down":
-			if m.cursor < len(m.keys)-1 {
-				m.cursor++
-			}
-		case "k", "up":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-		}
-	}
-	return m, nil
-}
-
-func (m model) View() string {
-	sb := strings.Builder{}
-
-	for i, k := range m.keys {
-		if i == m.cursor {
-			sb.WriteString(styleSelected.Render(k))
-		} else {
-			sb.WriteString(styleNormal.Render(k))
-		}
-		sb.WriteString("\n")
-	}
-	sb.WriteString(m.table.View())
-	return sb.String()
 }
--- redis.go
+++ redis.go
@@ -59,6 +59,111 @@ func (r *Rdb) Del(ctx context.Context, key string) error {
 	return r.client.Del(ctx, key).Err()
 }
 
+func (r *Rdb) Keys(ctx context.Context, pattern string) ([]string, error) {
+	keys, err := r.client.Keys(ctx, pattern).Result()
+	if err != nil {
+		return nil, err
+	}
+	return keys, nil
+}
+
 func (r *Rdb) Close() error {
 	return r.client.Close()
+}
+
+func (r *Rdb) Type(ctx context.Context, key string) (string, error) {
+	kind, err := r.client.Type(ctx, key).Result()
+	if err != nil {
+		return "", err
+	}
+	return kind, err
+}
+
+func (r *Rdb) HKeys(ctx context.Context, key string) ([]string, error) {
+	fields, err := r.client.HKeys(ctx, key).Result()
+	if err != nil {
+		return nil, err
+	}
+	return fields, nil
+}
+
+func (r *Rdb) Hget(ctx context.Context, key, field string) (string, error) {
+	val, err := r.client.HGet(ctx, key, field).Result()
+	if err != nil {
+		return "", err
+	}
+	return val, nil
+}
+
+func (r *Rdb) ValFromHash(ctx context.Context, key string) (map[string]string, error) {
+	m := make(map[string]string)
+
+	fields, err := r.HKeys(ctx, key)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, f := range fields {
+		val, err := r.Hget(ctx, key, f)
+		if err != nil {
+			return nil, err
+		}
+
+		m[f] = val
+	}
+
+	return m, nil
+}
+
+func (r *Rdb) ExtractVal(ctx context.Context, key, kind string) (interface{}, error) {
+	var val interface{}
+	var err error
+
+	switch kind {
+	case "string":
+		val, err = r.Get(ctx, key)
+		if err != nil {
+			return nil, err
+		}
+
+	case "hash":
+		val, err = r.ValFromHash(ctx, key)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return val, nil
+}
+
+func (r *Rdb) GetRecords(ctx context.Context, pattern string) ([]Record, error) {
+
+	keys, err := r.Keys(ctx, pattern)
+	if err != nil {
+		return nil, err
+	}
+
+	records := make([]Record, len(keys))
+
+	for i, key := range keys {
+		kind, err := r.Type(ctx, key)
+		if err != nil {
+			return nil, err
+		}
+
+		val, err := r.ExtractVal(ctx, key, kind)
+		if err != nil {
+			return nil, err
+		}
+
+		r := Record{
+			key:  key,
+			val:  val,
+			kind: kind,
+		}
+
+		records[i] = r
+	}
+
+	return records, nil
 }
